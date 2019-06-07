4eb23acb9c6530a018726fb8f233ab2a0511a3b9
Margot Konig <margot.konig@blackrock.com>
2019-06-07 16:06:33 +0000

    Added all methods to CLI file

--- lib/mkonig/cli/command_line_interface.rb
+++ lib/mkonig/cli/command_line_interface.rb
@@ -12,27 +12,77 @@ class CommandLineInterface
   def welcome 
     puts "Welcome to Margot's CLI Project to discover all of the open positions at Sweetgreen!"
     puts "To see all open positions, enter 'list positions'."
-    puts "To see all their offices with open positions, enter 'list offices'."
-    puts "To see all their departments with open positions, enter 'list departments'."
-    puts "To see all open positions in a particular office, enter 'list offices'."
-    puts "To count how many openings exist in a particular office, enter 'list offices'."
-    puts "To see all open positions in a particular department, enter 'list departments'."
-    puts "To count how many openings exist in a particular department, enter 'list departments'."
+    puts "To see all offices with open positions, enter 'list offices'."
+    puts "To see all departments with open positions, enter 'list departments'."
+    puts "To see all open positions in a particular office, enter 'list positions in an office'."
+    puts "To see all open positions in a particular department, enter 'list positions in a department'."
+    puts "To quit, type 'exit'."
+    command = ""
+    until command == "exit"
+      command = gets.chomp.downcase
+          if command == "list positions"
+              list_positions
+          elsif command == "list offices"
+              list_offices
+          elsif command == "list departments"
+              list_departments
+          elsif command == "list positions in an office"
+              list_positions_by_office
+          elsif  command == "list positions in a department"
+              list_positions_by_department
+          else  
+          end
+      end
   end 
   
   def list_positions
+        Position.all.each_with_index do |pos,index| 
+            puts "#{index+1}. #{pos.name} - #{pos.department.name} - #{song.office.name}" 
+        end
   end 
   
   def list_offices 
+        Office.all.each_with_index do |off,index| 
+            puts "#{index+1}. #{off.name}" 
+        end
   end 
   
   def list_departments 
+        Department.all.each_with_index do |dpt,index| 
+            puts "#{index+1}. #{dpt.name}" 
+        end
   end 
   
-  def count_positions
+  def list_positions_by_office
+    list_offices
+    puts "Please enter an office:"
+    input = gets.chomp.downcase
+      if office = Office.all.find {|off| off.name.downcase == input}
+          Position.all.each_with_index do |pos, index| 
+              if pos.office.name.downcase == input
+                  puts "#{index+1}. #{pos.name} - #{pos.department.name} - #{song.office.name}" 
+              else
+                #skip?
+              end
+          end
+      end
   end 
   
-  
+  def list_positions_by_department
+    list_departments
+    puts "Please enter a department:"
+    input = gets.chomp.downcase
+      if department = Department.all.find {|dpt| dpt.name.downcase == input}
+          Position.all.each_with_index do |pos, index| 
+              if pos.department.name.downcase == input
+                  puts "#{index+1}. #{pos.name} - #{pos.department.name} - #{song.office.name}" 
+              else
+                next
+                ## check whether next works this way
+              end
+          end
+      end
+  end 
 
   # def add_attributes_to_students
   #   Student.all.each do |student|
